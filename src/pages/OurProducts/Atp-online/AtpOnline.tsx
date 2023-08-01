67916d97e1c5b224fba26ec5830d2cebc8ed15bd
DzmitryYesis <yesis1988@mail.ru>
2023-08-01 19:25:35 +0200

    feat: create logic for products page

---
+++ src/pages/OurProducts/Atp-online/AtpOnline.tsx
@@ -0,0 +1,7 @@
+export const AtpOnline = () => {
+    return (
+        <div>
+            ATP-online
+        </div>
+    )
+}
--- src/pages/OurProducts/OurProducts.tsx
+++ src/pages/OurProducts/OurProducts.tsx
@@ -1,5 +1,31 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {RoutPath} from '../../enum';
+import {PageWrapper} from '../../components/PageWrapper/PageWrapper';
+import {AtpOnline} from './Atp-online/AtpOnline';
+
+type OurProductsType = 'ATP'
+
 export const OurProducts = () => {
+
+    const navigate = useNavigate();
+
+    const [content, setContent] = useState<OurProductsType>('ATP')
+
+    const mainPageHandler = () => {
+        navigate(RoutPath.MainPage)
+    }
+
     return (
-        <div>Our products</div>
+        <div>
+            {
+                content === 'ATP' && (
+                    <PageWrapper title={'АТП-онлайн'} leftBtnFunc={mainPageHandler}
+                                 rightBtn={false}>
+                        <AtpOnline/>
+                    </PageWrapper>
+                )
+            }
+        </div>
     )
 }
