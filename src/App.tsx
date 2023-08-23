6ecd34f23238ad375e1ec8a87bf7cde84867c0d4
DzmitryYesis <yesis1988@mail.ru>
2023-08-23 15:27:08 +0200

    refactor: add LinkItem to BurgerMenu.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import {Header} from './components/Header/Header';
 import {BrowserRouter} from 'react-router-dom';
 import {MainPage} from './pages/MainPage/MainPage';
 import {BurgerMenu} from './components/BurgerMenu/BurgerMenu';
+import {NavBarLinks, NavBarLinksType} from './components/Header/NavBarLinks';
+
+function createInitialState(data: NavBarLinksType[]) {
+    const initialState: Record<string, boolean> = {};
+    data.forEach((link) => {
+        initialState[link.path] = false;
+    });
+
+    return initialState;
+}
+
+function reducer(
+    state: Record<string, boolean>,
+    action: { type: string; payload: string }
+) {
+    const newInitialState = {...state};
+    switch (action.type) {
+        case 'ACTIVATE_LINK':
+            Object.keys(newInitialState).forEach(
+                (key) => (newInitialState[key] = false)
+            );
+            newInitialState[action.payload] = true;
+            return newInitialState;
+        case 'DEACTIVATE_LINK':
+            newInitialState[action.payload] = false;
+            return newInitialState;
+        default:
+            return state;
+    }
+}
 
 function App() {
 
     const [isOpen, setIsOpen] = useState(false)
+
+    const [state, dispatch] = useReducer(
+        reducer,
+        createInitialState(NavBarLinks)
+    );
+
+    const activeLinkHandler = (path: string) => {
+        if (!state[path]) {
+            dispatch({type: 'ACTIVATE_LINK', payload: path});
+        } else {
+            dispatch({type: 'DEACTIVATE_LINK', payload: path});
+        }
+    };
+
+    const currentURL = window.location.pathname
 
     const burgerMenuHandler = () => {
         setIsOpen(!isOpen)
@@ -14,8 +59,15 @@ function App() {
 
     return (
         <BrowserRouter>
-            <Header setIsOpen={burgerMenuHandler}/>
-            <BurgerMenu isOpen={isOpen} setIsOpen={burgerMenuHandler}/>
+            <Header
+                setIsOpen={burgerMenuHandler}
+                currentURL={currentURL}
+                activeLinkHandler={activeLinkHandler}/>
+            <BurgerMenu
+                isOpen={isOpen}
+                setIsOpen={burgerMenuHandler}
+                currentURL={currentURL}
+                activeLinkHandler={activeLinkHandler}/>
             <MainPage/>
             {/*<RootContainer/>*/}
             {/*<Footer/>*/}
--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,15 +1,24 @@
 import cn from 'classnames';
 import s from './BurgerMenu.module.scss';
 import cross from '../../assets/image/Header/cross.svg';
+import {NavBarLinks} from '../Header/NavBarLinks';
+import {LinkItem} from '../LinkItem/LinkItem';
 
 type BurgerMenuPropsType = {
     isOpen: boolean,
     setIsOpen: VoidFunction
+    activeLinkHandler: (path: string) => void
+    currentURL: string
 }
 
-export const BurgerMenu = ({isOpen, setIsOpen}: BurgerMenuPropsType) => {
+export const BurgerMenu = ({
+                               isOpen,
+                               setIsOpen,
+                               activeLinkHandler,
+                               currentURL
+                           }: BurgerMenuPropsType) => {
 
-    const burgerMenuHandler = ()=>{
+    const burgerMenuHandler = () => {
         setIsOpen()
     }
 
@@ -22,11 +31,13 @@ export const BurgerMenu = ({isOpen, setIsOpen}: BurgerMenuPropsType) => {
                 <img className={s.burger_open} src={cross} alt={'cross-menu'}
                      onClick={burgerMenuHandler}/>
                 <div className={s.menu_list}>
-                    <span>Наши продукты</span>
-                    <span>О нас</span>
-                    <span>Связатся с нами</span>
-                    <span>Делаем</span>
-                    <span>Сотрудничество</span>
+                    {
+                        NavBarLinks.map((item) => {
+                            return <LinkItem item={item} currentURL={currentURL}
+                                             key={item.path}
+                                             onActivateLink={activeLinkHandler}/>
+                        })
+                    }
                 </div>
             </div>
         </div>
