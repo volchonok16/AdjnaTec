3e34567a7ee4039436228a50d0510cf5f164414b
DzmitryYesis <yesis1988@mail.ru>
2023-09-03 20:27:01 +0200

    feat: add animation for program complex card

--- src/pages/OurProducts/Atp-online/ProgramComplexBlock/ProgramComplexBlock.tsx
+++ src/pages/OurProducts/Atp-online/ProgramComplexBlock/ProgramComplexBlock.tsx
@@ -3,54 +3,54 @@ import {ProgramComplexCard} from './ProgramComplexCard/ProgramComplexCard';
 
 type CardType = {
     number: number
-    title: string,
+    title: string[],
     description: string
 }
 
 const cardData: Record<string, CardType> = {
     1: {
         number: 1,
-        title: 'Журнал механика по выпуску транспорта',
+        title: ['Журнал механика по', 'выпуску транспорта'],
         description: 'Позволяет фиксировать выезд и заезд транспорта, замечания к его техническому состоянию'
     },
     2: {
         number: 2,
-        title: 'Журнал ремонта транспорта',
+        title: ['Журнал ремонта', 'транспорта'],
         description: 'Организация и оперативный контроль ремонта транспорта, регламентных ТО, учет шин и аккумуляторов, позволяет вовремя заказывать необходимые запчасти'
     },
     3: {
         number: 3,
-        title: 'Обработка путевых листов',
+        title: ['Обработка', 'путевых листов'],
         description: 'Контроль эксплуатации транспорта, расходов на него, контрольные и управленческие отчеты, печать актов выполненных работ и многое другое.'
     },
     4: {
         number: 4,
-        title: 'Табель',
+        title: ['Табель'],
         description: 'Контрольные и управленческие отчеты по персоналу во всех возможных разрезах. Применяются в том числе для начисления заработной платы водителям'
     },
     5: {
         number: 5,
-        title: 'Склад',
+        title: ['Склад'],
         description: 'Приход-расход ТМЦ, распределение со склада, контролирование расхода запчастей. Позволяет выполнять бухгалтерские проводки на основе данных склада'
     },
     6: {
         number: 6,
-        title: 'Разнарядка',
+        title: ['Разнарядка'],
         description: 'Позволяет простейшим образом создавать план выпуска техники и печатать путевые листы.'
     },
     7: {
         number: 7,
-        title: 'Заявочный модуль',
+        title: ['Заявочный', 'модуль'],
         description: 'Устанавливается на стороне ваших постоянных клиентов, позволяет автоматизировать создание заявок, которые тут же будут отражаться в модуле Разнарядка.'
     },
     8: {
         number: 8,
-        title: 'Управленческий модуль',
+        title: ['Управленческий', 'модуль'],
         description: 'Позволяет собрать в одном месте всю информацию, упростить формирование отчетов, отслеживать всю оперативную обстановку на предприятии из любого места,где есть интернет'
     },
     9: {
         number: 9,
-        title: 'Модуль водителя',
+        title: ['Модуль', 'водителя'],
         description: 'Позволяет водителю отмечать статус на рейсе (погрузка, опоздание, ожидание клиента и т. д.), отправлять фотографии и сообщения в чат диспетчера и обратно.'
     }
 }
@@ -65,12 +65,25 @@ export const ProgramComplexBlock = () => {
 ПРОГРАММНЫЙ КОМПЛЕКС СОСТОИТ ИЗ 9 <span className={s.title_text_blue}>ВЗАИМОСВЯЗАН-НЫХ МОДУЛЕЙ</span>
 </span>
                 </div>
-                <ProgramComplexCard number={cardData[1].number}/>
-                <ProgramComplexCard number={cardData[2].number}/>
-                <ProgramComplexCard number={cardData[3].number}/>
+                <ProgramComplexCard
+                    fontSize={'29px'}
+                    number={cardData[1].number}
+                    titles={cardData[1].title}
+                    description={cardData[1].description}/>
+                <ProgramComplexCard
+                    number={cardData[2].number}
+                    titles={cardData[2].title}
+                    description={cardData[2].description}/>
+                <ProgramComplexCard
+                    number={cardData[3].number}
+                    titles={cardData[3].title}
+                    description={cardData[3].description}/>
             </div>
             <div className={s.row_container}>
-                <ProgramComplexCard number={cardData[4].number}/>
+                <ProgramComplexCard
+                    number={cardData[4].number}
+                    titles={cardData[4].title}
+                    description={cardData[4].description}/>
                 <div className={s.text_card}>
 <span className={s.description_text}>
     Эти модули обеспечивают взаимодействие, контроль и управление всеми службами АТП, максимально возможная автоматизация
@@ -78,13 +91,28 @@ export const ProgramComplexBlock = () => {
 из управления
 </span>
                 </div>
-                <ProgramComplexCard number={cardData[5].number}/>
-                <ProgramComplexCard number={cardData[6].number}/>
+                <ProgramComplexCard
+                    number={cardData[5].number}
+                    titles={cardData[5].title}
+                    description={cardData[5].description}/>
+                <ProgramComplexCard
+                    number={cardData[6].number}
+                    titles={cardData[6].title}
+                    description={cardData[6].description}/>
             </div>
             <div className={s.row_container}>
-                <ProgramComplexCard number={cardData[7].number}/>
-                <ProgramComplexCard number={cardData[8].number}/>
-                <ProgramComplexCard number={cardData[9].number}/>
+                <ProgramComplexCard
+                    number={cardData[7].number}
+                    titles={cardData[7].title}
+                    description={cardData[7].description}/>
+                <ProgramComplexCard
+                    number={cardData[8].number}
+                    titles={cardData[8].title}
+                    description={cardData[8].description}/>
+                <ProgramComplexCard
+                    number={cardData[9].number}
+                    titles={cardData[9].title}
+                    description={cardData[9].description}/>
                 <button className={s.button}>
                     <span className={s.button_text}>Попробовать</span>
                 </button>
--- src/pages/OurProducts/Atp-online/ProgramComplexBlock/ProgramComplexCard/ProgramComplexCard.tsx
+++ src/pages/OurProducts/Atp-online/ProgramComplexBlock/ProgramComplexCard/ProgramComplexCard.tsx
@@ -1,15 +1,57 @@
 import s from './ProgramComplexCard.module.scss';
+import React, {useState} from 'react';
+import cn from 'classnames';
 
 type ProgramComplexCardPropsType = {
-    number: number
+    fontSize?: string
+    number: number,
+    titles: string[],
+    description: string
 }
 
-export const ProgramComplexCard = ({number}: ProgramComplexCardPropsType) => {
+export const ProgramComplexCard = ({
+                                       number,
+                                       titles,
+                                       description,
+                                       fontSize
+                                   }: ProgramComplexCardPropsType) => {
+
+    const [isHover, setIsHover] = useState(false)
+
+    const hoverHandler = () => {
+        setIsHover(!isHover)
+    }
+
     return (
-        <div className={s.program_complex_card_container}>
+        <div className={s.program_complex_card_container} onMouseOver={hoverHandler}
+             onMouseOut={hoverHandler}>
             <span className={s.number}>
                 /{number}
             </span>
+            <div className={cn(s.text_container, {
+                [s.text_container_hover]: isHover
+            })}>
+                <div className={cn(s.title_wrapper, {
+                    [s.title_wrapper_hover]: isHover
+                })}>
+                    {
+                        titles.map((title) => {
+                            return (
+                                <span className={cn(s.title, {
+                                    [s.title_hover]: isHover
+                                })}
+                                      style={{fontSize: fontSize}}
+                                      key={title}>{title}</span>
+                            )
+                        })
+                    }
+                </div>
+                <span className={cn(s.description, {
+                    [s.description_hover]: isHover
+                })}>
+                        {description}
+                    </span>
+            </div>
         </div>
     )
 }
