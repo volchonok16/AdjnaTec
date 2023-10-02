15e1a22662b0571dbbdb5ae3168139a1a47f816e
DzmitryYesis <yesis1988@mail.ru>
2023-10-02 12:56:14 +0200

    feat: add tablet design for AutomationBlock.tsx

--- src/pages/OurProducts/Atp-online/AutomationBlock/AutomationBlock.tsx
+++ src/pages/OurProducts/Atp-online/AutomationBlock/AutomationBlock.tsx
@@ -5,6 +5,15 @@ import {TryButton} from '../../../../components/TryButton/TryButton';
 export const AutomationBlock = () => {
     return (
         <div className={s.automation_block_container}>
+            <div className={s.text_1_tablet_wrapper}>
+                <div className={s.text_container}>
+                    <span className={s.text_1}>
+                    <span className={s.text_1_blue}>Наш девиз</span>: — «Все, что может делать машина,</span>
+                    <span className={s.text_1}> <span
+                        className={s.text_1_blue}>должна делать машина</span>, а не человек».
+                    </span>
+                </div>
+            </div>
             <img className={s.img} src={automation} alt={'automation-img'}/>
             <div className={s.right_block}>
                 <div className={s.text_block}>
@@ -18,6 +27,16 @@ export const AutomationBlock = () => {
                 </div>
                 <TryButton/>
             </div>
+            <div className={s.tablet_content_container}>
+                <img className={s.img_tablet} src={automation} alt={'automation-img'}/>
+                <div className={s.right_block_tablet}>
+                    <span className={s.text_2_tablet}>
+                        В связи с высокой степенью автоматизации процедур, алгоритмов перепроверки данных, Атп-Онлайн исключается человеческий фактор из управления.
+                    </span>
+                    <TryButton/>
+                </div>
+            </div>
+
         </div>
     )
 }
