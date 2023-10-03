be4f2fa92e6e477fe2796824d84d1b82e35dd262
DzmitryYesis <yesis1988@mail.ru>
2023-10-03 12:03:08 +0200

    feat: add tablet design for ProgramComplexBlock.tsx

--- src/pages/OurProducts/Atp-online/ProgramComplexBlock/ProgramComplexBlock.tsx
+++ src/pages/OurProducts/Atp-online/ProgramComplexBlock/ProgramComplexBlock.tsx
@@ -121,6 +121,68 @@ export const ProgramComplexBlock = () => {
                     <span className={s.button_text}>Попробовать</span>
                 </button>
             </div>
+            <div className={s.text_container_tablet}>
+                <div className={s.title_wrapper_tablet}>
+                    <span className={s.title_tablet}>ПРОГРАММНЫЙ КОМПЛЕКС СОСТОИТ</span>
+                    <span className={s.title_tablet}>ИЗ 9 <span
+                        className={s.title_tablet_blue}>ВЗАИМОСВЯЗАННЫХ МОДУЛЕЙ</span></span>
+                </div>
+                <div className={s.description_wrapper_tablet}>
+<span className={s.description_tablet}>Эти модули обеспечивают взаимодействие, контроль
+и управление всеми службами АТП, максимально возможная автоматизация  и полное исключение человеческого фактора из управления</span>
+                </div>
+            </div>
+            <div className={s.card_container_tablet}>
+                <div className={s.card_row_tablet}>
+                    <ProgramComplexCard
+                        number={cardData[1].number}
+                        titles={cardData[1].title}
+                        description={cardData[1].description}/>
+                    <ProgramComplexCard
+                        number={cardData[2].number}
+                        titles={cardData[2].title}
+                        description={cardData[2].description}/>
+                    <ProgramComplexCard
+                        number={cardData[3].number}
+                        titles={cardData[3].title}
+                        description={cardData[3].description}/>
+                </div>
+                <div className={s.card_row_tablet}>
+                    <ProgramComplexCard
+                        number={cardData[4].number}
+                        titles={cardData[4].title}
+                        description={cardData[4].description}/>
+                    <ProgramComplexCard
+                        number={cardData[5].number}
+                        titles={cardData[5].title}
+                        description={cardData[5].description}/>
+                    <ProgramComplexCard
+                        number={cardData[6].number}
+                        titles={cardData[6].title}
+                        description={cardData[6].description}/>
+                </div>
+                <div className={s.card_row_tablet}>
+                    <ProgramComplexCard
+                        number={cardData[7].number}
+                        titles={cardData[7].title}
+                        description={cardData[7].description}/>
+                    <ProgramComplexCard
+                        number={cardData[8].number}
+                        titles={cardData[8].title}
+                        description={cardData[8].description}/>
+                    <div className={s.card_with_button}>
+                        <ProgramComplexCard
+                            height={'min(232px, calc(170px + 62 * ((100vw - 768px) / 255)))'}
+                            descriptionFontSize={'min(15px, calc(10.5px + 3.5 * ((100vw - 768px) / 255)))'}
+                            number={cardData[9].number}
+                            titles={cardData[9].title}
+                            description={cardData[9].description}/>
+                        <button className={s.button_tablet} onClick={redirectHandler}>
+                            <span className={s.button_text_tablet}>Попробовать</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
