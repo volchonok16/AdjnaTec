1f3be4f8db91b3dcc68cc5075ca5eeba7638666e
DzmitryYesis <yesis1988@mail.ru>
2023-10-10 13:14:18 +0200

    refactor: change company name on Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,12 @@ export const Footer = () => {
     return (
         <div className={s.footer_container}>
             <div className={s.block_1}>
-                <span className={s.block_1_text_1}>{`<AdjnaTech/>`}</span>
+                <span className={s.block_1_text_1}>{`<AdjnaTec/>`}</span>
                 <span className={s.block_1_text_2}>Эффективные решения для перезагрузки вашего бизнеса</span>
             </div>
             <div className={s.block_2}>
                 <div className={s.line}/>
-                <span className={s.block_2_text_1}>@ AdjnaTech 2023</span>
+                <span className={s.block_2_text_1}>@ AdjnaTec 2023</span>
             </div>
         </div>
     )
